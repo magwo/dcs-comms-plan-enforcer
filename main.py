2d258a27821e9f63dbd6197b1ca45ad632a91677
Magnus Wolffelt <magnus.wolffelt@avanza.se>
2021-07-21 00:28:11 +0200

    Add working handlers that update frequencies

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from handlers import handle_plane_group
 import dcs
 import sys
 
@@ -8,6 +9,8 @@ from comms_plan import UHF_PLAN, VIGGEN_PLAN, INTRA_PLAN
 
 m = dcs.Mission()
 
+print(sys.argv)
+
 if len(sys.argv) < 2:
     print("#                                           #")
     print("# Usage: provide .miz filename to modify    #")
@@ -16,31 +19,33 @@ if len(sys.argv) < 2:
 
 filename = sys.argv[1]
 print("Attempting to load mission file", filename)
-load_result = m.load_file(filename)
-# print("Load result", load_result)
+m.load_file(filename)
 
 
 def processUnit(plane_or_helicopter: PlaneGroup):
     return
 
 
-for coalition in m.coalition:
-    print(coalition)
+# for coalition in m.coalition:
+#     print(coalition)
 
+num_updated = 0
 for country_id in m._COUNTRY_IDS:
     country = m.country_by_id(country_id)
-    
+
     if country:
         for group in country.plane_group:
-            print("Group", group)
-            for unit in group.units:
-                # print("Unit type", unit.type)
-                if unit.type == "AJS37":
-                    print("Unit type", unit.type)
-                    print("Radio", unit.radio)
-                    unit.set_radio_channel_preset(1, 1, 265.25)
-            
-        # for plane in country.planes:
-        #     print("Plane", plane)
+            # print("Group", group)
+            num_updated += handle_plane_group(group)
+            # for unit in group.units:
+            #     print("Unit type", unit.type)
 
-# m.save("output.miz")
+print("{} units were updated/enforced".format(num_updated))
+
+if len(sys.argv) >= 3:
+    out_filename = sys.argv[2]
+    print("Attempting to save to {}...".format(out_filename))
+    m.save(out_filename)
+    print("Saved to {}".format(out_filename))
+else:
+    print("Dry run was completed")
